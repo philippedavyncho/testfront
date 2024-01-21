3f8ba398f6d7bef4284cab29ec25472887f795bc
philippedavyncho <nchodavyphilippe@gmail.com>
2024-01-21 00:25:17 +0000

    cp de6

--- src/App.js
+++ src/App.js
@@ -35,14 +35,12 @@ export default function App(){
             <header>
                 <Header />
             </header>
-            <div className="sectionCont">
             <section>
                 <Routes>
                     <Route path='/' element={<Donnees />}/>
                     <Route path='/:produitId' element={<Detail/>}/>
                 </Routes>
             </section>
-            </div>
             <footer>
                 <Footer />
             </footer>
