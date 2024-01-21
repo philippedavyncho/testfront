61f9ac4db3848279b93d11766c99e938d354cda4
philippedavyncho <nchodavyphilippe@gmail.com>
2024-01-21 01:48:37 +0000

    cp ed3

--- src/Composants/LeftSlider.js
+++ src/Composants/LeftSlider.js
@@ -10,10 +10,10 @@ export default function Slider(){
     return(
         <div class="carouselA">
           <div class="carousel-slideA">
-            <img src={lateral} alt={lateral}/>
-            <img src={lateral1} alt={lateral1}/>
-            <img src={lateral2} alt={lateral2}/>
-            <img src={lateral5} alt={lateral5}/>
+            <img src={lateral} alt={lateral} style={{width:"100%"}}/>
+            <img src={lateral1} alt={lateral1} style={{width:"100%"}}/>
+            <img src={lateral2} alt={lateral2} style={{width:"100%"}}/>
+            <img src={lateral5} alt={lateral5} style={{width:"100%"}}/>
           </div>
         </div>
     )
