1701fc1adb82f040ef2bda5896d476231020e564
philippedavyncho <nchodavyphilippe@gmail.com>
2024-01-21 01:11:52 +0000

    dedj

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ export default function App(){
             <header>
                 <Header />
             </header>
+            <div className='navsec'>
             <nav>
                 <OrderProvider>
                   <Menu />
@@ -42,6 +43,7 @@ export default function App(){
                     <Route path='/:produitId' element={<Detail/>}/>
                 </Routes>
             </section>
+            </div>
             <footer>
                 <Footer />
             </footer>
